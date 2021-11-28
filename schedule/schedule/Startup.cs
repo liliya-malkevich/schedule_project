3b9c707f29547b8d8f63cf8861fb0c6d49e6b4e1
liliya-malkevich <l.malkevich12@gmail.com>
2021-11-28 18:09:43 +0300

    added service and connection to bd

--- schedule/schedule/Startup.cs
+++ schedule/schedule/Startup.cs
@@ -5,17 +5,23 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using schedule.Service;
 
 namespace schedule
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration) => Configuration = configuration;
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //связываем конфиг с json
+            Configuration.Bind("Project", new Config());
             services.AddControllersWithViews().SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_3_0).AddSessionStateTempDataProvider(); 
         }
 
